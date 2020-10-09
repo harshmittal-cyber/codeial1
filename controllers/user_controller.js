0172d11cc0eabf1dbb5f7905f46c57b8d6d8975a
Harsh Mittal <mittalharsh4321@gmail.com>
2020-10-09 13:43:21 +0530

    implementing partials

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,5 @@
 module.exports.profile=function(req,res){
-    return res.end("<h1>User profile</h1>");
+    return res.render('user_profile',{
+        title:"My profile"
+    })
 }
--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use('/',require('./routes/index.js'));
 app.set('view engine','ejs');
 app.set('views','./views');
 
-
 app.listen(port,function(err){
     if(err){
         console.log('Error in running a server',err);
