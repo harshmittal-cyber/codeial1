c3391826012f591e7c3ce4bd9a36c7d1c90c9387
Harsh Mittal <mittalharsh4321@gmail.com>
2020-10-09 12:37:23 +0530

    setup the express router

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express=require('express');
 const app=express();
-
 const port=8000;
+
+//use express router
+app.use('/',require('./routes/index.js'));
+
+
+
 
 app.listen(port,function(err){
     if(err){
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express=require('express');
+//setting up router
+const router=express.Router();
+
+
+console.log('router loaded');
+module.exports=router;
