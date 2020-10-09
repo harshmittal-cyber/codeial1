1fa60d7ceab17c72e28732d64b0ec096aaa0ca11
Harsh Mittal <mittalharsh4321@gmail.com>
2020-10-09 13:07:10 +0530

    install ejs and set view engine

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port=8000;
 //this call router and then router call controller for / then we get a response
 app.use('/',require('./routes/index.js'));
 
-
+//setting up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
 app.listen(port,function(err){
