de27deecc28485575b58f556b79b7483d2fa9e4a
Harsh Mittal <mittalharsh4321@gmail.com>
2020-10-09 13:54:25 +0530

    creating a layout

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const expresslayouts=require('express-ejs-layouts');
+
+//using layouts and it is a wrapper of body for which controller is render 
+app.use(expresslayouts);
+
 
 //use express router
 //this call router and then router call controller for / then we get a response
