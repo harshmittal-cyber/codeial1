99c9690feb9d88ff824468d96419aaf62e06e37c
Harsh Mittal <mittalharsh4321@gmail.com>
2020-10-09 20:04:17 +0530

    creating a schema

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+
+//creating a schema
+const userschema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    }
+
+},{//use for created at and updated at in database
+    timestamps:true
+});
+
+const user=mongoose.model('User',userschema);
