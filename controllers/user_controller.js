ad458d7853abd70440a9e999b485d67a234239a1
Harsh Mittal <mittalharsh4321@gmail.com>
2020-10-14 10:48:04 +0530

    File Upload: Saving File from params

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,4 @@
+const { use } = require('passport');
 const user = require('../models/user');
 const User=require('../models/user');
 module.exports.profile=function(req,res){
@@ -9,14 +10,43 @@ module.exports.profile=function(req,res){
     })   
 }
 
-module.exports.update=function(req,res){
-    if(req.user.id==req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body,function(err,user){
+module.exports.update=async function(req,res){
+    // if(req.user.id==req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body,function(err,user){
+    //         req.flash('success','Updated') 
+    //         return res.redirect('back');
+    //     })
+    // }else{
+    //     req.flash('error','Unauthorized'); 
+    //     return res.status(401).send('Unauthorized');
+    // }
+    if(req.user.id == req.params.id){
+        try{
+            let user=await User.findById(req.params.id);
+
+            User.uploadedAvatar(req,res,function(err){
+                if(err){console.log('*******Multer Error:',err)}
+
+                user.name=req.body.name;
+                user.email=req.body.email;
+
+                if(req.file){
+                    //this is saving the path of the uploaded file into the avatr fielf into the user
+                    user.avatar=user.avatarPath + '/' +req.file.filename
+                }
+                user.save();
+                return res.redirect('back');
+            });
+
+        }catch(err){
+            req.flash('error',err);
             return res.redirect('back');
-        })
+        }
     }else{
+        req.flash('error','Unauthorized'); 
         return res.status(401).send('Unauthorized');
     }
+
 }
 
 //render the signup page
--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
 const mongoose=require('mongoose');
+const multer=require('multer');
+//used for where file path is
+const path=require('path');
+const AVATAR_PATH=path.join('/uploads/users/avatar');
 
 //creating a schema
 const userschema=new mongoose.Schema({
@@ -14,11 +18,32 @@ const userschema=new mongoose.Schema({
     name:{
         type:String,
         required:true
+    },
+    avatar:{
+        type:String
     }
 
 },{//use for created at and updated at in database
     timestamps:true
 });
+
+//multer npm code from documentation
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+
+//static
+//single is used for only 1 file is uploaded at instance
+userschema.statics.uploadedAvatar=multer({storage:storage}).single('avatar');
+
+//made this AVATAR_PATH publically available
+userschema.statics.avatarPath=AVATAR_PATH;  
+
 
 const User=mongoose.model('User',userschema);
 
