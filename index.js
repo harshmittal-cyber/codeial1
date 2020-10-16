29f615d7d0cc85f85c834a78f1dd3a58aaf3f178
Harsh Mittal <mittalharsh4321@gmail.com>
2020-10-16 10:47:31 +0530

    Setting up passport-JWT

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db=require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
-
+const passportJWT=require('./config/passport-jwt-startegy');
 //mongo store and it require session argument which is in bracket
 const MongoStore=require('connect-mongo')(session);
 
