c8b3419aaef9476a5ba865a538a8c076e0c43241
Harsh Mittal <mittalharsh4321@gmail.com>
2020-10-23 12:16:21 +0530

    Message are sent and received using socket.io

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -11,9 +11,15 @@ module.exports.chatSockets=function(socketServer){
         socket.on('join_room',function(data){
             console.log('joining request rec',data);
 
+            //add the client to the room
             socket.join(data.chatroom);
             //this tell us to evry user that this user join the chat room
             io.in(data.chatroom).emit('user_joined',data);
+        });
+
+        //change:: detect send message and broadcast to everyone in the room
+        socket.on('send_message',function(data){
+            io.in(data.chatroom).emit('receive_message',data);
         })
     })
 }
